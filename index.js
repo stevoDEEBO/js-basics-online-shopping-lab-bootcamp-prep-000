784a66c366b4f04e279272a00c43f4b26e9ca95e
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-01-31 17:30:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,20 +18,19 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
-  //if (cart.length === 0) {
-    //console.log("Your shopping cart is empty.")
-  //}
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  }
   
-  //else if (cart.length === 1) {
-    //console.log("In your cart, you have " + Object.keys(cart[0]) + " at $" + cart[0][Object.keys(cart[0])] + ".")
-  //}
+  else if (cart.length === 1) {
+    console.log("In your cart, you have " + Object.keys(cart[0]) + " at $" + cart[0][Object.keys(cart[0])] + ".")
+  }
   
-  //else if (cart.length === 2){
-    //console.log("In your cart, you have " + Object.keys(cart[0]) + " at $" + cart[0][Object.keys(cart[0])] + " and " + Object.keys(cart[1]) + " at $" + cart[1][Object.keys(cart[1])] + ".")
-  //}
+  else if (cart.length === 2){
+    console.log("In your cart, you have " + Object.keys(cart[0]) + " at $" + cart[0][Object.keys(cart[0])] + " and " + Object.keys(cart[1]) + " at $" + cart[1][Object.keys(cart[1])] + ".")
+  }
   
-  //else {
-  if (cart.length > 2) {  
+  else {
     var items = []
     for (var i = 0; i < cart.length-1; i++) {
     items.push(` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`)
